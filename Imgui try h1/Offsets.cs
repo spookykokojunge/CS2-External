b595e122463440501e538c97748bff23023f89ba
Spooky <157039814+spookykokojunge@users.noreply.github.com>
2024-06-14 20:55:40 +0200

    Huge update

---
+++ Imgui try h1/Offsets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imgui_try_h1
+{
+    // Define offsets as constants
+    public static class Offsets
+    {
+        public const int dwEntityList = 0x19B49B8;
+        public const int dwViewMatrix = 0x1A16A70;
+        public const int dwLocalPlayerPawn = 0x181A998;
+        public const int m_vOldOrigin = 0x1274;
+        public const int m_iTeamNum = 0x3C3;
+        public const int m_lifeState = 0x328;
+        public const int m_hPlayerPawn = 0x7DC;
+        public const int m_vecViewOffset = 0xC50;
+        public const int m_iHealth = 0x324;
+        public const int m_entitySpottedState = 0x2278;
+        public const int m_bSpotted = 0x8;
+        public const int m_iszPlayerName = 0x630;
+        public const int m_pGameSceneNode = 0x308;
+        public const int m_modelState = 0x170;
+    }
+
+}
--- Imgui try h1/Renderer.cs
+++ Imgui try h1/Renderer.cs
@@ -12,10 +12,10 @@ namespace Imgui_try_h1
 {
     public class Renderer : Overlay
     {
-        //Render Variables
-        public Vector2 screenSize = new Vector2(1920, 1080); //ScreenSize of your Screen
+        // Render Variables
+        public Vector2 screenSize = new Vector2(1920, 1080); // Screen size
 
-        //entities copy , using more Threads is a safer method.
+        // Entities, using concurrent collection for thread safety
         private ConcurrentQueue<Entity> entities = new ConcurrentQueue<Entity>();
         private Entity localPlayer = new Entity();
         private readonly object entityLock = new object();
@@ -23,241 +23,261 @@ namespace Imgui_try_h1
         // GUI Elements Variables
         private bool enableESP = true;
         private bool enableLine = false;
-        public bool enableHealth = true;
-        public bool enableName = true;
-        private bool enableVisibilityCheck = true;
-        public bool enableFOV = false;
+        private bool enableHealth = true;
+        private bool enableName = false;
+        private bool enableVisibilityCheck = false;
+        private bool enableBox = false;
+        private bool enableBone = true;
+        private bool enableTeammateESP = false; // New variable to enable/disable ESP for teammates
 
-        public int fov = 90; //default fov
-
-        //collors
-        private Vector4 enemyColor = new Vector4(1, 0, 0, 1); //Default Red
-        private Vector4 teamColor = new Vector4(0, 1, 0, 1); //Default Green
-        private Vector4 hiddenColor = new Vector4(0, 0, 0, 1); //Default Black
-        private Vector4 nameColor = new Vector4(1, 1, 1, 1); //Default White
-        private Vector4 boneColor = new Vector4(1, 1, 1, 1); //Default White
+        // Colors
+        private Vector4 enemyColor = new Vector4(1, 0, 0, 1); // Red
+        private Vector4 teamColor = new Vector4(0, 1, 0, 1); // Green
+        private Vector4 hiddenColor = new Vector4(0, 0, 0, 1); // Black
+        private Vector4 nameColor = new Vector4(1, 1, 1, 1); // White
+        private Vector4 boneColor = new Vector4(1, 1, 1, 1); // White
+        private Vector4 lowHealthColor = new Vector4(1, 1, 0, 1); // Yellow
+        private Vector4 criticalHealthColor = new Vector4(1, 0.5f, 0, 1); // Orange
 
         float boneThickness = 4;
 
-        //draw list
+        // Draw list
         ImDrawListPtr drawList;
 
         protected override void Render()
         {
+            ImGuiStylePtr style = ImGui.GetStyle();
+
+            style.WindowBorderSize = 1f;
+            style.WindowRounding = 8f;
+            style.Colors[(int)ImGuiCol.Border] = new Vector4(252 / 255f, 106 / 255f, 0 / 255f, 1f);
+            style.Colors[(int)ImGuiCol.TitleBgActive] = new Vector4(252 / 255f, 106 / 255f, 0 / 255f, 1f);
+            style.Colors[(int)ImGuiCol.Text] = new Vector4(1,1,1,1f);
+            style.Colors[(int)ImGuiCol.Tab] = new Vector4(252 / 255f, 106 / 255f, 0 / 255f, 1f);
+            style.Colors[(int)ImGuiCol.TabActive] = new Vector4(252 / 255f, 106 / 255f, 0 / 255f, 1f);
+            style.Colors[(int)ImGuiCol.TabHovered] = new Vector4(232 / 255f, 90 / 255f, 0 / 255f, 1f);
+
             ImGui.Begin("Zynx - Sookyisnice");
 
-            // Begin Tab Bar
             if (ImGui.BeginTabBar("SettingsTabs"))
             {
-                // ESP Tab
                 if (ImGui.BeginTabItem("ESP"))
                 {
-                    ImGui.Checkbox("ESP", ref enableESP);
-                    ImGui.Checkbox("Name", ref enableName);
-                    ImGui.Checkbox("VisCheck", ref enableVisibilityCheck);
-                    ImGui.Checkbox("Lines", ref enableLine);
-                    ImGui.Checkbox("Health", ref enableHealth);
+                    RenderESPSettings();
                     ImGui.EndTabItem();
                 }
 
-                // FOV Tab
-                if (ImGui.BeginTabItem("FOV"))
-                {
-                    ImGui.Checkbox("FOV", ref enableFOV);
-                    ImGui.SliderInt("FOV", ref fov, 90, 140);
-                    ImGui.EndTabItem();
-                }
-
-                // Color Tab
                 if (ImGui.BeginTabItem("Colors"))
                 {
-                    ImGui.ColorEdit4("##Enemy", ref enemyColor);
-                    ImGui.ColorEdit4("##Bone", ref boneColor);
-                    ImGui.ColorEdit4("##Name", ref nameColor);
+                    RenderColorSettings();
                     ImGui.EndTabItem();
                 }
-
-
 
                 ImGui.EndTabBar();
             }
 
-
-            //Draw Overlay
             DrawOverlay(screenSize);
             drawList = ImGui.GetWindowDrawList();
 
-            //draw stuff
             if (enableESP)
             {
                 foreach (var entity in entities)
                 {
-                    if (EntityOnScreen(entity) && localPlayer.team != entity.team)
+                    bool isTeammate = localPlayer.team == entity.team;
+                    if (EntityOnScreen(entity) && (!isTeammate || (isTeammate && enableTeammateESP)))
                     {
-                        //Check If Entity On Screen
-                        if (EntityOnScreen(entity))
-                        {
-                            //Draw Methods
-                            DrawBox(entity);
-                            DrawLine(entity);
-                            DrawHealthBar(entity);
-                            DrawName(entity, 20);
-                            DrawBones(entity);
-
-                        }
-
+                        DrawEntity(entity);
                     }
                 }
             }
         }
 
-        //Check Position
-        bool EntityOnScreen(Entity entity)
+        private void RenderESPSettings()
         {
-            if (entity.position2D.X > 0 && entity.position2D.X < screenSize.X && entity.position2D.Y > 0 && entity.position2D.Y < screenSize.Y)
-            {
-                return true;
-            }
-            return false;
+            ImGui.Checkbox("ESP", ref enableESP);
+            ImGui.Checkbox("Box", ref enableBox);
+            ImGui.Checkbox("Bone", ref enableBone);
+            ImGui.Checkbox("Name", ref enableName);
+            ImGui.Checkbox("VisCheck", ref enableVisibilityCheck);
+            ImGui.Checkbox("Lines", ref enableLine);
+            ImGui.Checkbox("Health", ref enableHealth);
+            ImGui.Checkbox("Teammate ESP", ref enableTeammateESP); // New checkbox for teammate ESP
         }
 
+        private void RenderColorSettings()
+        {
+            ImGui.ColorEdit4("Enemy", ref enemyColor, ImGuiColorEditFlags.PickerMask | ImGuiColorEditFlags.NoInputs);
+            ImGui.ColorEdit4("Bone", ref boneColor, ImGuiColorEditFlags.PickerMask | ImGuiColorEditFlags.NoInputs);
+            ImGui.ColorEdit4("Name", ref nameColor, ImGuiColorEditFlags.PickerMask | ImGuiColorEditFlags.NoInputs);
+            ImGui.ColorEdit4("Low Health", ref lowHealthColor, ImGuiColorEditFlags.PickerMask | ImGuiColorEditFlags.NoInputs);
+            ImGui.ColorEdit4("Critical Health", ref criticalHealthColor, ImGuiColorEditFlags.PickerMask | ImGuiColorEditFlags.NoInputs);
+        }
 
-        //Drawing Methods
+        private void DrawEntity(Entity entity)
+        {
+            DrawBox(entity);
+            DrawLine(entity);
+            DrawHealthBar(entity);
+            DrawName(entity, 20);
+            DrawBones(entity);
+        }
+
+        private bool EntityOnScreen(Entity entity)
+        {
+            return entity.position2D.X > 0 && entity.position2D.X < screenSize.X &&
+                   entity.position2D.Y > 0 && entity.position2D.Y < screenSize.Y;
+        }
 
         private void DrawBones(Entity entity)
         {
-            uint uintColor = ImGui.ColorConvertFloat4ToU32(boneColor);
+            if (!enableBone) return;
 
-            float currentBoneThickness = boneThickness / entity.distance;
+            uint color = ImGui.ColorConvertFloat4ToU32(boneColor);
+            float thickness = boneThickness / entity.distance;
 
-            drawList.AddLine(entity.bones2d[1], entity.bones2d[2], uintColor, currentBoneThickness); //neckt to head
-            drawList.AddLine(entity.bones2d[1], entity.bones2d[3], uintColor, currentBoneThickness); //neck to left shoulder
-            drawList.AddLine(entity.bones2d[1], entity.bones2d[6], uintColor, currentBoneThickness); //neck to right shoulder
-            drawList.AddLine(entity.bones2d[3], entity.bones2d[4], uintColor, currentBoneThickness); //shoulderRight to arm
-            drawList.AddLine(entity.bones2d[6], entity.bones2d[7], uintColor, currentBoneThickness); //shoulderLeft to arm
-            drawList.AddLine(entity.bones2d[4], entity.bones2d[5], uintColor, currentBoneThickness); //armLeft to handLeft
-            drawList.AddLine(entity.bones2d[7], entity.bones2d[8], uintColor, currentBoneThickness); //armRight to rightHand
-            drawList.AddLine(entity.bones2d[1], entity.bones2d[0], uintColor, currentBoneThickness); //neck to waist
-            drawList.AddLine(entity.bones2d[0], entity.bones2d[9], uintColor, currentBoneThickness); //waist to kneeLeft
-            drawList.AddLine(entity.bones2d[0], entity.bones2d[11], uintColor, currentBoneThickness); //waist to kneeRight
-            drawList.AddLine(entity.bones2d[9], entity.bones2d[10], uintColor, currentBoneThickness); //kneeLeft to feetLeft
-            drawList.AddLine(entity.bones2d[11], entity.bones2d[12], uintColor, currentBoneThickness); //knee to feetRight
-            drawList.AddCircle(entity.bones2d[2], 3 + currentBoneThickness, uintColor); //circle on head
+            drawList.AddLine(entity.bones2d[1], entity.bones2d[2], color, thickness);
+            drawList.AddLine(entity.bones2d[1], entity.bones2d[3], color, thickness);
+            drawList.AddLine(entity.bones2d[1], entity.bones2d[6], color, thickness);
+            drawList.AddLine(entity.bones2d[3], entity.bones2d[4], color, thickness);
+            drawList.AddLine(entity.bones2d[6], entity.bones2d[7], color, thickness);
+            drawList.AddLine(entity.bones2d[4], entity.bones2d[5], color, thickness);
+            drawList.AddLine(entity.bones2d[7], entity.bones2d[8], color, thickness);
+            drawList.AddLine(entity.bones2d[1], entity.bones2d[0], color, thickness);
+            drawList.AddLine(entity.bones2d[0], entity.bones2d[9], color, thickness);
+            drawList.AddLine(entity.bones2d[0], entity.bones2d[11], color, thickness);
+            drawList.AddLine(entity.bones2d[9], entity.bones2d[10], color, thickness);
+            drawList.AddLine(entity.bones2d[11], entity.bones2d[12], color, thickness);
+            drawList.AddCircle(entity.bones2d[2], 3 + thickness, color);
         }
-
 
         private void DrawName(Entity entity, int yOffset)
         {
-            if (enableName)
-            {
-                Vector2 textLocation = new Vector2(entity.viewPositon2D.X, entity.viewPositon2D.Y - yOffset); //Get render location of name
-                drawList.AddText(textLocation, ImGui.ColorConvertFloat4ToU32(nameColor), $"{entity.name}"); //Draw on screen
-            }
+            if (!enableName) return;
 
+            Vector2 textLocation = new Vector2(entity.viewPositon2D.X, entity.viewPositon2D.Y - yOffset);
+            drawList.AddText(textLocation, ImGui.ColorConvertFloat4ToU32(nameColor), entity.name);
         }
-
-
-
 
         private void DrawHealthBar(Entity entity)
         {
-            if (enableHealth)
+            if (!enableHealth) return;
+
+            // Calculate bar height
+            float entityHeight = entity.position2D.Y - entity.viewPositon2D.Y;
+
+            // Get box location
+            float boxLeft = entity.viewPositon2D.X - entityHeight / 3;
+            float boxRight = entity.position2D.X + entityHeight / 3;
+
+            // Calculate health bar width
+            float barPercentWidth = 0.05f; // 5% or 1/20 of box width
+            float barPixelWidth = barPercentWidth * (boxRight - boxLeft);
+
+            // Calculate bar height after health
+            float barHeight = entityHeight * (entity.health / 100f);
+
+            // Calculate bar rectangle, two vectors
+            Vector2 barTop = new Vector2(boxLeft - barPixelWidth, entity.position2D.Y - barHeight);
+            Vector2 barBottom = new Vector2(boxLeft, entity.position2D.Y);
+
+            // Determine bar color based on health
+            Vector4 barColor;
+            if (entity.health < 10)
             {
-                //calculate bar height
-                float entityHeight = entity.position2D.Y - entity.viewPositon2D.Y;
-
-
-                //get box location
-                float boxLeft = entity.viewPositon2D.X - entityHeight / 3;
-                float boxRight = entity.position2D.X + entityHeight / 3;
-
-
-                //calculate health bar width
-                float barPercentWidth = 0.05f; // 5% ore 1/20 of box width
-                float barPixelWidth = barPercentWidth * (boxRight - boxLeft);
-
-
-                //calculate bar height after health
-                float barHeight = entityHeight * (entity.health / 100f);
-
-
-                //calculate bar rectange, two vertors
-                Vector2 barTop = new Vector2(boxLeft - barPixelWidth, entity.position2D.Y - barHeight);
-                Vector2 barBottom = new Vector2(boxLeft, entity.position2D.Y);
-
-
-                Vector4 barColor = new Vector4(0, 1, 0, 1);
-
-
-                //draw health bar
-                drawList.AddRectFilled(barTop, barBottom, ImGui.ColorConvertFloat4ToU32(barColor));
-
+                barColor = new Vector4(1, 0, 0, 1); // Red
+            }
+            else if (entity.health < 25)
+            {
+                barColor = new Vector4(1, 0.5f, 0, 1); // Orange
+            }
+            else if (entity.health < 50)
+            {
+                barColor = new Vector4(1, 1, 0, 1); // Yellow
+            }
+            else if (entity.health < 75)
+            {
+                barColor = new Vector4(0.5f, 1, 0, 1); // Lime Green
+            }
+            else
+            {
+                barColor = new Vector4(0, 1, 0, 1); // Green
             }
 
+            // Draw health bar
+            drawList.AddRectFilled(barTop, barBottom, ImGui.ColorConvertFloat4ToU32(barColor));
 
+            // Draw health text
+            string healthText = entity.health.ToString();
+            Vector2 textSize = ImGui.CalcTextSize(healthText);
+            Vector2 textPosition = new Vector2(barTop.X - textSize.X - 2, barTop.Y - textSize.Y / 2);
 
+            // Determine text color based on health
+            Vector4 textColor;
+            if (entity.health < 20)
+            {
+                textColor = new Vector4(1, 0, 0, 1); // Red
+            }
+            else if (entity.health < 40)
+            {
+                textColor = new Vector4(1, 0.5f, 0, 1); // Orange
+            }
+            else if (entity.health < 60)
+            {
+                textColor = new Vector4(1, 1, 0, 1); // Yellow
+            }
+            else if (entity.health < 80)
+            {
+                textColor = new Vector4(0.5f, 1, 0, 1); // Lime Green
+            }
+            else
+            {
+                textColor = new Vector4(1, 1, 1, 1); // White
+            }
+
+            // Draw health text
+            drawList.AddText(textPosition, ImGui.ColorConvertFloat4ToU32(textColor), healthText);
         }
+
 
 
         private void DrawBox(Entity entity)
         {
-            //Calculate Box Height
+            if (!enableBox) return;
+
             float entityHeight = entity.position2D.Y - entity.viewPositon2D.Y;
-
-            //Calculate Box Dimensions
             Vector2 rectTop = new Vector2(entity.viewPositon2D.X - entityHeight / 3, entity.viewPositon2D.Y);
-
             Vector2 rectBottom = new Vector2(entity.position2D.X + entityHeight / 3, entity.position2D.Y);
 
-            //Get Correct Color
             Vector4 boxColor = localPlayer.team == entity.team ? teamColor : enemyColor;
 
-            //Check If Player Is Visible Ore Hidden
             if (enableVisibilityCheck)
-                boxColor = entity.spotted == true ? boxColor : hiddenColor; //Set Color To Black If Not Spotted
-
+                boxColor = entity.spotted ? boxColor : hiddenColor;
 
             drawList.AddRect(rectTop, rectBottom, ImGui.ColorConvertFloat4ToU32(boxColor));
         }
 
-
-
-
         private void DrawLine(Entity entity)
         {
-            if (enableLine)
-            {
-                //Get Correct Color
-                Vector4 lineColor = localPlayer.team == entity.team ? teamColor : enemyColor;
+            if (!enableLine) return;
 
-                //Draw Line
-                drawList.AddLine(new Vector2(screenSize.X / 2, screenSize.Y), entity.position2D, ImGui.ColorConvertFloat4ToU32(lineColor));
-
-            }
-
+            Vector4 lineColor = localPlayer.team == entity.team ? teamColor : enemyColor;
+            drawList.AddLine(new Vector2(screenSize.X / 2, screenSize.Y), entity.position2D, ImGui.ColorConvertFloat4ToU32(lineColor));
         }
 
-
-
-
-
-
-        //transfer entity methods
-
-
-        public void UpdateEntities(IEnumerable<Entity> newEntities) // update entities
+        public void UpdateEntities(IEnumerable<Entity> newEntities)
         {
             entities = new ConcurrentQueue<Entity>(newEntities);
         }
 
-
-        public void UpdateLocalPlayer(Entity newEntity) //Update Localplayer
+        public void UpdateLocalPlayer(Entity newEntity)
         {
-            localPlayer = newEntity;
+            lock (entityLock)
+            {
+                localPlayer = newEntity;
+            }
         }
 
-
-        public Entity GetLocalPlayer() //Get Localplayer
+        public Entity GetLocalPlayer()
         {
             lock (entityLock)
             {
@@ -265,12 +285,10 @@ namespace Imgui_try_h1
             }
         }
 
-
-        void DrawOverlay(Vector2 screeSize) // Overlay Window 
+        private void DrawOverlay(Vector2 screenSize)
         {
-
             ImGui.SetNextWindowSize(screenSize);
-            ImGui.SetNextWindowPos(new Vector2(0, 0)); // in the beninguing
+            ImGui.SetNextWindowPos(Vector2.Zero);
             ImGui.Begin("Overlay", ImGuiWindowFlags.NoDecoration
                 | ImGuiWindowFlags.NoBackground
                 | ImGuiWindowFlags.NoBringToFrontOnFocus
@@ -278,8 +296,7 @@ namespace Imgui_try_h1
                 | ImGuiWindowFlags.NoInputs
                 | ImGuiWindowFlags.NoCollapse
                 | ImGuiWindowFlags.NoScrollbar
-                | ImGuiWindowFlags.NoScrollWithMouse
-                );
+                | ImGuiWindowFlags.NoScrollWithMouse);
         }
     }
 }
